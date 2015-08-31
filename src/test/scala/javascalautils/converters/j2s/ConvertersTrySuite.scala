1071d8b361eace0a1755dfcb1e69fde946751ee6
Peter Nerg <peter.nerg@gmail.com>
2015-08-31 19:16:32 +0200

    #14 Added tests for the Failure converter

---
+++ src/test/scala/javascalautils/converters/j2s/ConvertersTrySuite.scala
@@ -0,0 +1,34 @@
+/**
+ *  Copyright 2015 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package javascalautils.converters.j2s
+
+import org.scalatest.FunSuite
+
+import javascalautils.{ None => JNone, Option => JOption, Some => JSome }
+import javascalautils.converters.j2s.Converters._
+import javascalautils.{Try => JTry, Success => JSuccess, Failure => JFailure}
+
+/**
+ * Test suite for Converters Try/Success/Failure conversions.
+ * @author Peter Nerg
+ */
+class ConvertersTrySuite extends FunSuite {
+  test("Test asScalaFailure") {
+    val failure = asScalaFailure(new JFailure(new Exception("Error, terror")))
+    assert(failure.isFailure)
+    assertResult("Error, terror")(failure.failed.get.getMessage)
+  }
+}
