c1783accf37544bc513bad434531e9cea302c4f3
Peter Nerg <peter.nerg@gmail.com>
2015-09-20 08:16:50 +0200

    added JavaScalaUtilConvertersSuite

--- src/test/scala/javascalautils/converters/JavaScalaUtilConvertersSuite.scala
+++ src/test/scala/javascalautils/converters/JavaScalaUtilConvertersSuite.scala
@@ -18,39 +18,25 @@ package javascalautils.converters
 import org.scalatest.FunSuite
 
 import javascalautils.{ None => JNone, Some => JSome, Option => JOption }
-import javascalautils.converters.JavaScalaUtilImplicits._
+import javascalautils.converters.JavaScalaUtilConverters._
 
 /**
- * Test suite for JavaScalaUtilImplicits. <br>
+ * Test suite for JavaScalaUtilConverters. <br>
  * This will only test a few use cases to verify the import.
  * @author Peter Nerg
  */
-class JavaScalaUtilImplicitsSuite extends FunSuite {
-  val expected = "Peter was here"
+class JavaScalaUtilConvertersSuite extends FunSuite {
+  val expected = "Some is never None"
 
-  test("Java Option-None as Scala") {
-    val jnone: JOption[String] = new JNone()
-    val option = jnone.asScala()
-    assert(option.isEmpty)
-  }
-
-  test("Java Option-Some as Scala") {
-    val jsome: JOption[String] = new JSome(expected)
-    val option = jsome.asScala()
+  test("Test asScalaOption with Some") {
+    val option = asScalaOption(new JSome(expected))
     assert(option.isDefined)
     assertResult(expected)(option.get)
   }
 
-  test("Java None as Scala") {
-    val none = new JNone[String]().asScala()
-    assert(none.isEmpty)
+  test("Test asJavaOption with Some") {
+    val joption = asJavaOption(Some(expected))
+    assert(joption.isDefined)
+    assertResult(expected)(joption.get)
   }
-
-  test("Java Some as Scala") {
-    val some = new JSome(expected).asScala()
-
-    assert(some.isDefined)
-    assertResult(expected)(some.get)
-  }
-
 }
