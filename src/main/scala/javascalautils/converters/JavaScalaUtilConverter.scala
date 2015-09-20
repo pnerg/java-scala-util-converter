9084aa9a1af5387e64a461983970f0c19f4355a7
Peter Nerg <peter.nerg@gmail.com>
2015-09-20 07:49:40 +0200

    refactored JavaScalaConverter
    removed redundant traits

--- src/main/scala/javascalautils/converters/JavaScalaUtilConverter.scala
+++ src/main/scala/javascalautils/converters/JavaScalaUtilConverter.scala
@@ -17,60 +17,18 @@
 package javascalautils.converters
 
 /**
- * Allows for implicit conversion of the supported data types.
+ * Allows for implicit conversion of the supported data types. <br>
+ * Is an aggregate of the [[j2s.Implicits]] and [[s2j.Implicits]].
  * @author Peter Nerg
  * @since 1.0
  */
-object JavaScalaUtilImplicits extends Implicits
-
-/**
- * Is an aggregate of the j2s.Implicits and s2j.Implicits.
- * @author Peter Nerg
- * @since 1.0
- */
-trait Implicits extends j2s.Implicits with s2j.Implicits
+object JavaScalaUtilImplicits extends j2s.Implicits with s2j.Implicits
 
 /**
  * Allows for explicit conversion of the supported data types. <br>
- * ==Code Examples==
- * ===Java -> Scala===
- * Using the javascalutils.Option/Some/None classes as example
- * {{{
- * import javascalautils.{ None => JNone, Option => JOption, Some => JSome }
- * import javascalautils.converters.JavaScalaUtilConverters._
- * 
- * val jnone: JOption[String] = new JNone()
- * val option = asScalaOption(jnone)
- * 
- * val none = asScalaNone(new JNone[String]())
- * 
- * val some = asScalaSome(new JSome("Some is never None"))
- * }}}
- * ===Scala -> Java===
- * Using the scala.Option/Some/None classes as example
- * {{{
- * import javascalautils.converters.JavaScalaUtilConverters._
- * 
- * val jnone = asJavaNone(None)
- * 
- * val jsome = asJavaSome(Some("Some is never None"))
- * 
- * val joption = asJavaOption(Some("Some is never None"))
- * }}}
- * <br>
- * For further examples refer to the various Java -> Scala converters: <br>
- * [[javascalautils.converters.j2s.OptionConverters]] <br>
- * [[javascalautils.converters.j2s.TryConverters]] <br>
- * [[javascalautils.converters.j2s.EitherConverters]] <br>
- * [[javascalautils.converters.j2s.FutureConverters]] <br>
+ * Is an aggregate of the [[j2s.Converters]] and [[s2j.Converters]]. <br>
+ * Refer to the various methods for code examples.
  * @author Peter Nerg
  * @since 1.0
  */
-object JavaScalaUtilConverters extends Converters
-
-/**
- * Is an aggregate of the j2s.Converters and s2j.Converters.
- * @author Peter Nerg
- * @since 1.0
- */
-trait Converters extends j2s.Converters with s2j.Converters
+object JavaScalaUtilConverters extends j2s.Converters with s2j.Converters
