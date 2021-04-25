d2788f5d02c59296496ea69ff3d70b1d1e432f21
Peter Nerg <peter.nerg@gmail.com>
2021-04-25 00:29:17 +0000

    other: update scalatest from 3.2.7 to 3.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies extends AutoPlugin {
      * Test dependencies
      * ------------------------------
      */
-    val scalatest = "org.scalatest" %% "scalatest" % "3.2.7"
+    val scalatest = "org.scalatest" %% "scalatest" % "3.2.8"
   }
 
 }
